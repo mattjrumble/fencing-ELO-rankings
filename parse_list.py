50e5629eceda0d000cabab6d02630c54ed2d8d9d
Matt Rumble <mattjrumble@gmail.com>
2018-08-14 22:20:07 +0100

    2016 results into json

---
+++ parse_list.py
@@ -0,0 +1,20 @@
+"""
+Parse the results list,
+"""
+
+import json
+            
+results_list = 'results-2016-17.json'
+
+with open(results_list) as f:
+    content = f.read()
+    
+comps = json.loads(content)
+
+for comp in comps:
+    print("Name: {}".format(comp['name']))
+    print("Date: {}".format(comp['date']))
+    for event in comp['events']:
+        if 'supported' in event and not event['supported']:
+            continue
+        print("Weapon: {}. Url: {}".format(event['weapon'], event['url']))
